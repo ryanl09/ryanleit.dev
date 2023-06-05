5294eff6add0d476d0777defc8a2d701ca2d31c8
ryanl09 <67206661+ryanl09@users.noreply.github.com>
2023-06-05 01:05:27 -0400

    fixmpadding issue on mobile

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -17,7 +17,7 @@ export default function Experience() {
                     </span>
                 </a>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-6 px-8'>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6 px-4 md:px-8'>
                 <div>
                     <div className='selected font-medium text-lg'>Work Experience</div>
                     <Work
