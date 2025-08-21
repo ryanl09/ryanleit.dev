1aad231a49ea42842647b9b5dd1814ce0df2ba9c
ryanl09 <71403615+ryanl09@users.noreply.github.com>
2025-08-21 14:41:11 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,16 @@ const nextConfig = {
         pathname: '**',
       }
     ]
-  }
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production'
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
